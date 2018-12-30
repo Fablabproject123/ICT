419df15c801730d289f816a7efb1ae2fc1f8d2de
Tuấn <tuan10281028@gmail.com>
2018-12-30 15:14:35 +0700

    edit color pages

--- wp-content/themes/eightstore-lite/footer.php
+++ wp-content/themes/eightstore-lite/footer.php
@@ -12,46 +12,35 @@
 </div><!-- #content -->
 <div class="clear"></div>
 <footer id="colophon" class="site-footer" role="contentinfo">
-	<?php
+    <?php
 
-	if(is_active_sidebar('footer-2')){
-		?>
-		<section id="section-footer" class="clear">
-			<div class="container">
+    if (is_active_sidebar('footer-2')) {
+        ?>
+        <section id="section-footer" class="clear">
+            <div class="container">
                 <div class="store-wrapper">
                     <div class="row">
                         <?php dynamic_sidebar('footer-2'); ?>
                     </div>
                 </div>
             </div>
-		</section>
-		<?php
-	}
-	?>
-	<div class="store-footer">
-		<div class="store-wrapper">
-			<div class="footer-copyrt">
-				<div class="site-info">
-					<?php
-					if(get_theme_mod('footer_copyright_text') && get_theme_mod('footer_copyright_text')!=""){
-						echo wp_kses_post(get_theme_mod('footer_copyright_text'));
-					}?>
-
-				</div><!-- .site-info -->
-
-				<!-- Social Icons -->
-				<?php if(get_theme_mod('social_icons_in_footer') && get_theme_mod('social_icons_in_footer')!='0'){ ?>
-					<div class="es-social-footer">
-						<?php do_action('eightstore_lite_social_links'); ?>
-					</div>
-					<?php } ?>
-
-					<!-- Payment Partner Logos -->
-					<?php do_action('eightstore_lite_payment_partner_logos');?>
-				</div>
-			</div>
-		</div>
-	</footer><!-- #colophon -->
+        </section>
+        <?php
+    }
+    ?>
+    <div class="store-footer">
+        <div class="store-wrapper">
+            <div class="footer-copyrt">
+                <div class="site-info">
+                    <?php
+                    if (get_theme_mod('footer_copyright_text') && get_theme_mod('footer_copyright_text') != "") {
+                        echo wp_kses_post(get_theme_mod('footer_copyright_text'));
+                    } ?>
+                </div><!-- .site-info -->
+            </div>
+        </div>
+    </div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 <div id="es-top"></div>
 <?php wp_footer(); ?>
